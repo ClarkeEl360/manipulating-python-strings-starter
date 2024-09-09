8348aa77c0664d64316386982dbf4618b463ceb2
ClarkeEl360 <ClarkeEl360@northwested.org>
2024-09-09 11:22:59 -0400

    Create # Exercise 2 - 3 (Personal Message).py

---
+++ # Exercise 2 - 3 (Personal Message).py
@@ -0,0 +1,11 @@
+# Exercise 2 - 3  (Personal Message)
+
+print("Dripamold")
+
+#  Exercise 2 - 5 (Famous Quote)
+
+print("There is no such thing as try, there is only do or do not.")
+
+# Exercise 2 - 6 (Famous Quote 2)
+
+print("With great power comes great responsibility")
